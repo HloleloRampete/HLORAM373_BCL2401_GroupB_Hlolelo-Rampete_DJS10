f7253b843fb457423aedce7eb775b14eb4d2ad05
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-11 13:29:43 +0200

    "Added useEffect hook to fetch data from API and update state"

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,6 +4,27 @@ import { useEffect, useState } from "react-dom"
 export default function App() {
   const [posts, setPosts] = useState([])
   const [error, setError] = useState(null)
+
+  React.useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if (!response.ok) {
+          throw new Error(`API request failed with status ${response.status}`);
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+  
+    fetchPosts();
+  }, [])
+
+  
+  
+
   return (
     <h1>Hi</h1>
   )
